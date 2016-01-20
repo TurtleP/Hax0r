4f05489482e90c93b5d6caad508a6ba306cc61ff
TurtleP <jeremy.postelnek@gmail.com>
2016-01-20 08:23:59 -0500

    fixed credits

--- states/credits.lua
+++ states/credits.lua
@@ -9,36 +9,20 @@ function credits_update(dt)
 	local speed = 40
 
 	for k = 1, #creditsy do
-		if creditsy[#creditsy][2] == "top" then
-			if creditsy[#creditsy][1] < (gameFunctions.getHeight() / 2 - consoleFont:getHeight(credits[#credits - 1]) / 2) + consoleFont:getHeight(credits[#credits - 1]) / 2 then
-				speed = 0
-			end
-		end
-
 		creditsy[k][1] = creditsy[k][1] - speed * dt
-
-		if creditsy[k][1] < -24 and creditsy[k][2] == "bottom" then
-			creditsy[k][1] = gameFunctions.getHeight()
-			creditsy[k][2] = "top"
-		end
 	end
 end
 
 function credits_draw()
-	love.graphics.setScreen("bottom")
-
 	love.graphics.setColor(255, 255, 255, 128)
-	love.graphics.draw(hax0r, love.graphics.getWidth() / 2 - hax0r:getWidth() / 2, love.graphics.getHeight() / 2 - hax0r:getHeight() / 2)
+	love.graphics.draw(hax0r, gameFunctions.getWidth() / 2 - hax0r:getWidth() / 2, gameFunctions.getHeight() / 2 - hax0r:getHeight() / 2)
 
 	love.graphics.setColor(255, 255, 255, 255)
 
 	love.graphics.setFont(consoleFont)
 	for k = 1, #credits do
-		love.graphics.setScreen(creditsy[k][2])
 		love.graphics.print(credits[k], gameFunctions.getWidth() / 2 - consoleFont:getWidth(credits[k]) / 2, creditsy[k][1])
 	end
-
-	love.graphics.setScreen("bottom")
 end
 
 function credits_mousepressed(x, y, button)
